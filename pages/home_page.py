f2eb9f16e52601e984b786420da9b46bcec8bad4
Mikołaj Mazur <mikolajmaz185@gmail.com>
2024-06-06 17:24:51 +0200

    Dodanie brancha 10 i zawartosci zadania zaliczeniowego

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,11 +1,11 @@
 from selenium.webdriver.common.by import By
-
 from pages.messages_page import MessagesPage
 
 
 class HomePage:
     user_email = (By.CSS_SELECTOR, ".user-info small")
     mail_icon = (By.CLASS_NAME, 'icon_mail')
+    admin_icon = (By.CSS_SELECTOR, '.header_admin')
 
     def __init__(self, browser):
         self.browser = browser
@@ -16,3 +16,6 @@ class HomePage:
     def click_mail_icon(self):
         self.browser.find_element(*self.mail_icon).click()
         return MessagesPage(self.browser)
+
+    def click_admin_panel(self):
+        self.browser.find_element(*self.admin_icon).click()
